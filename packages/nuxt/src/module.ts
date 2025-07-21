fc6b3c5e4a198096696006958e688d2cf0100ba9
Harlan Wilton <harlan@harlanzw.com>
2025-07-21 14:47:23 +1000

    fix(nuxt): v4 compatibility

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -12,7 +12,7 @@ export default defineNuxtModule<ModuleOptions>({
     version,
     configKey: 'mdream',
     compatibility: {
-      nuxt: '^3.0.0',
+      nuxt: '>=3.0.0',
     },
   },
   defaults: {
